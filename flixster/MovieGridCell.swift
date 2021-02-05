f2eb10ff8cdf5e68513ccf8d177dd530a4af9b14
yazeed505 <54512668+yazeed505@users.noreply.github.com>
2021-02-05 11:16:27 -0600

    -User can tap a cell to see more details about a particular movie. -User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by MAC on 2/5/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
